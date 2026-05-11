884b4cfa3434b2fdf57bea3b8a953107bb8171c6
davidarafienaGH <thedavidarafiena@gmail.com>
2026-05-11 12:57:38 +0100

    Today

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, I am learning Java");
+		System.out.print("Oh wow, Learning Java is fun. ");
+		System.out.print("Hurray, I am Happy. ");
+		System.out.printf("Java was created in %d%n",1992);
+		System.out.printf("Java was made free for %.2f dollars",0.0000);
+	}
+	
+}
